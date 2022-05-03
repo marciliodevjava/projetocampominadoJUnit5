6f3eec726feeb4f8cd1e84c00b99840ca4b26bb7
m-arc <m-arc@192.168.1.8>
2022-05-02 22:59:26 -0300

    Class Campo

--- src/modelo/Campo.java
+++ src/modelo/Campo.java
@@ -11,16 +11,32 @@ public class Campo {
 	private boolean aberto = false;
 	private boolean minado = false;
 	private boolean marcado = false;
-	
-	private List<Campo> visinhos = new ArrayList<>();
+
+	private List<Campo> vizinhos = new ArrayList<>();
 
 	Campo(int linha, int coluna) {
 		this.linha = linha;
 		this.coluna = coluna;
 	}
-	
-	boolean adicionarVisinho(Campo visinho) {
-		
+
+	boolean adicionarVisinho(Campo vizinho) {
+		boolean linhaDiferente = this.linha != vizinho.linha;
+		boolean colunhaDiferente = this.coluna != vizinho.coluna;
+		boolean diagonal = linhaDiferente && colunhaDiferente;
+
+		int deltaLinha = Math.abs(this.linha - vizinho.linha);
+		int deltaColunha = Math.abs(this.coluna - vizinho.coluna);
+		int deltaGeral = deltaColunha + deltaLinha;
+
+		if (deltaGeral == 1 && !diagonal) {
+			vizinhos.add(vizinho);
+			return true;
+		} else if (deltaGeral == 2 && diagonal) {
+			vizinhos.add(vizinho);
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 }
