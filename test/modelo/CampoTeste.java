34cbdefc4400556dc60196aafcf1d8bcb425407e
m-arc <m-arc@192.168.1.8>
2022-05-02 23:07:25 -0300

    Teste JUnit5 Class Campo

---
+++ test/modelo/CampoTeste.java
@@ -0,0 +1,17 @@
+package modelo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class CampoTeste {
+
+	private Campo campo = new Campo(3, 3);
+
+	@Test
+	void testeVisinhoRealDistancia1() {
+		Campo vizinho = new Campo(3, 2);
+		boolean resultado = campo.adicionarVisinho(vizinho);
+		assertTrue(resultado);
+	}
+}
