31ce8221dd3666d13fa8dac485a2adfc830ed078
m-arc <m-arc@192.168.1.8>
2022-05-04 09:28:38 -0300

    Class Aplicação

--- src/br/com/cod3r/campominado/Aplicacao.java
+++ src/br/com/cod3r/campominado/Aplicacao.java
@@ -8,6 +8,12 @@ public class Aplicacao {
 
 		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
 		
+		tabuleiro.abrir(3, 3);
+		tabuleiro.alterarMarcacao(4, 4);
+		tabuleiro.alterarMarcacao(4, 5);
+		tabuleiro.alterarMarcacao(2, 5);
+		tabuleiro.alterarMarcacao(2, 4);
+		
 		System.out.println(tabuleiro);
 	}
 }
