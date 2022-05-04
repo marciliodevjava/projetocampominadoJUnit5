50be39312310e7b8a062152ac2836cc061d0c402
m-arc <m-arc@192.168.1.8>
2022-05-04 17:04:50 -0300

    Class Aplicação

--- src/br/com/cod3r/campominado/Aplicacao.java
+++ src/br/com/cod3r/campominado/Aplicacao.java
@@ -1,19 +1,14 @@
 package br.com.cod3r.campominado;
 
 import modelo.Tabuleiro;
+import visao.TabuleiroConsole;
 
 public class Aplicacao {
 
 	public static void main(String[] args) {
 
-		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
-		
-		tabuleiro.abrir(3, 3);
-		tabuleiro.alterarMarcacao(4, 4);
-		tabuleiro.alterarMarcacao(4, 5);
-		tabuleiro.alterarMarcacao(2, 5);
-		tabuleiro.alterarMarcacao(2, 4);
-		
-		System.out.println(tabuleiro);
+		Tabuleiro tabuleiro = new Tabuleiro(6,6,6);
+		new TabuleiroConsole(tabuleiro);
+			
 	}
 }
