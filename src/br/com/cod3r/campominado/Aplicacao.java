5d190c0bcf17cf3fb613b1f5d8614b25d7b42181
m-arc <m-arc@192.168.1.8>
2022-05-04 08:33:57 -0300

    Class Aplicação

---
+++ src/br/com/cod3r/campominado/Aplicacao.java
@@ -0,0 +1,13 @@
+package br.com.cod3r.campominado;
+
+import modelo.Tabuleiro;
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+
+		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
+		
+		System.out.println(tabuleiro);
+	}
+}
